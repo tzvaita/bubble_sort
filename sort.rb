4ee4b8cca5163d3a9eb8ea1555ea71d436bc6869
tzvaita <tzvaita@gmail.com>
2020-01-20 22:27:47 +0200

    Fixing rubocop whitespaces offenses

--- sort.rb
+++ sort.rb
@@ -1,38 +1,36 @@
-
 def bubble_sort(array)
   n = array.length - 1
-  (array.length-1).times {
+  (array.length - 1).times do
     swapped = false
-    n.times { |i|
-      if array[i] > array[i+1]
-        array[i] , array[i+1] = array[i+1], array[i] 
+    n.times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
-    }
+    end
     break unless swapped
+
     n -= 1
-  }
+    end
   return array
 end
 
 def bubble_sort_by(array)
   n = array.length - 1
-  (array.length-1).times {
+  (array.length - 1).times do
     swapped = false
-    n.times { |i|
-      if yield(array[i] , array[i+1]) > 0
-        array[i] , array[i+1] = array[i+1], array[i] 
+    n.times do |i|
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
-    }
+    end
     break unless swapped
+
     n -= 1
-  }
+end
   return array
 end
-  
 
-
-
-print bubble_sort([3,2,1]) , "\n"
+print bubble_sort([3, 2, 1]), "\n"
 print bubble_sort_by(["pato", "camion", "papatzul"]) {|left, right| left.length - right.length}
